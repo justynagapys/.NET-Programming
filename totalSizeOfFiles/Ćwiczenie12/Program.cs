08a28df715711a736d24ae3dcc56de9ce252e447
Justyna Gapys <justynagapys0310@gmail.com>
2021-05-02 21:29:49 +0200

    lab4Commit3

---
+++ totalSizeOfFiles/Ćwiczenie12/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace Ćwiczenie12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj ścieżkę do wybranego katalogu: ");
+            string directoryPath = Console.ReadLine();
+
+            DirectoryInfo dinfo = new DirectoryInfo(directoryPath);
+
+            long filesSize = 0;
+
+            FileInfo[] allfiles = dinfo.GetFiles();
+            foreach (FileInfo finfo in allfiles)
+            {
+                filesSize = filesSize + finfo.Length;
+            }
+
+            Console.WriteLine($"Sumaryczna wartość plików w katalogu {directoryPath} wynosi: {filesSize} bajtów.");
+        }
+    }
+}
