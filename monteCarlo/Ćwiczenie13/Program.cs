0d95b69c134897b6e8be68c34d7e94e951430d59
Justyna Gapys <justynagapys0310@gmail.com>
2021-05-02 21:56:13 +0200

    lab4Commit4

---
+++ monteCarlo/Ćwiczenie13/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace Ćwiczenie13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj nazwę pliku do utworzenia:");
+            string fileName = Console.ReadLine();
+
+            FileInfo finfo = new FileInfo(fileName + ".txt");
+            FileStream fstream = finfo.Create();
+
+            using (var swriter = new StreamWriter(fstream))
+            {
+                swriter.WriteLine("Przybliżona wartość liczby PI metodą Monte Carlo wynosi: " + MonteCarlo());
+            }
+            fstream.Close();
+
+            Console.WriteLine("Przybliżona wartość liczby PI metodą Monte Carlo została obliczona.");
+        }
+
+        private static double MonteCarlo()
+        {
+            double piNumber = 0;          
+            long hits = 0;
+
+            Random random = new Random();
+            for (long i = 0; i < 100000000; ++i)
+            {
+                double x = random.NextDouble();
+                double y = random.NextDouble();
+                if (x * x + y * y < 1.0) 
+                    hits++;
+            }
+            return piNumber = 4.0 * hits / 100000000;
+        }
+    }
+}
