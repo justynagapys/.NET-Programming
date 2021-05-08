a02e88adf8cbc2e798f51b40cfd1ca9afee53e7a
Justyna Gapys <justynagapys0310@gmail.com>
2021-05-08 14:02:03 +0200

    lab5Commit2

---
+++ abstractClass/Ćwiczenie15/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ćwiczenie15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Second second = new Second();
+            second.f();
+            second.r();
+        }
+    }
+    public abstract class First
+    {
+        public abstract void f();
+        public virtual void r()
+        {
+            Console.WriteLine("Metoda r klasy First");
+        }
+    }
+
+    public class Second : First
+    {
+        public override void f()
+        {
+            Console.WriteLine("Metoda f klasy Second");
+        }
+
+        public override void r()
+        {
+            Console.WriteLine("Metoda r klasy Second");
+        }
+    }
+}
