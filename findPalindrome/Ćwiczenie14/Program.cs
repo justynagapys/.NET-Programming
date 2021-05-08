5d88de3426415ee3505201d8530ea301a413b8b4
Justyna Gapys <justynagapys0310@gmail.com>
2021-05-08 13:22:46 +0200

    lab5Commit1

---
+++ findPalindrome/Ćwiczenie14/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Ćwiczenie14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Znalezione palindromy: ");
+
+            using (StreamReader sr = new StreamReader("C:\\Users\\justy\\source\\repos\\Ćwiczenie14\\palindromy.txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Palindrom p = new Palindrom();
+                    Console.WriteLine(p.findPalindrome(line)); 
+                }
+            }
+        }
+    }
+
+    public class Palindrom
+    {
+        private string palindrom;
+        public string findPalindrome(string fileLine)
+        {
+            string[] wordsTab = fileLine.Split(" ");
+            foreach (var word in wordsTab)
+            {
+                char[] reverseWordArray = word.ToCharArray();
+                Array.Reverse(reverseWordArray);
+                string reverseWord = new string (reverseWordArray);
+                if (reverseWord == word)
+                {
+                    palindrom = word;
+                }
+            }
+            return palindrom;
+        }
+    }
+}
