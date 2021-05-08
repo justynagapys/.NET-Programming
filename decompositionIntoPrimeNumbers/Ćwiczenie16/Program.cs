592297ee87854fe7da6676d2d85f6249f0d1185c
Justyna Gapys <justynagapys0310@gmail.com>
2021-05-08 14:33:33 +0200

    lab5Commit3

---
+++ decompositionIntoPrimeNumbers/Ćwiczenie16/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Ćwiczenie16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę do rozłożenia: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.Write($"Czynniki pierwsze dla {number} to: ");
+            int x = 2;
+            int y = (int)Math.Sqrt(number);
+            while (x <= y)
+            {
+                while ((number % x) == 0)
+                {
+                    number /= x;
+                    y = (int)Math.Sqrt(number);
+                    Console.Write(x + " ");
+                }
+                x++;
+            }
+            if (number > 1) Console.Write(number+ " ");
+        }
+    }
+}
